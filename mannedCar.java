748f157a5cc1b004ec8221d3635b55d7061a43f0
Yipeng <352244533@qq.com>
2016-03-09 19:34:05 -0700

    租车系统version1
    这是我写的代码，欢迎大家修改，提意见

---
+++ mannedCar.java
@@ -0,0 +1,15 @@
+/*
+这是一个车的子类，叫做载人车，除了继承车的特性之外，
+它还有一个特殊的属性，叫做载客量。
+*/
+public class mannedCar extends ACar{
+	int manned;
+	public mannedCar(String name,int price, int manned){
+		this.name=name;
+		this.price=price;
+		this.manned=manned;
+	}
+	public void info(){
+		System.out.println(name+"\t\t"+price+"元/天\t载人："+manned+"人");
+	}
+}
